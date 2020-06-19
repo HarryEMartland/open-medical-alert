2be073415856553ec6de2a56541a57940fb94bbe
HarryEMartland <HarryEMartland@users.noreply.github.com>
2020-06-19 18:31:25 +0000

    deploy: 97a0bda71724b956aa4698711ee2f5b58f7bc80d

---
+++ precache-manifest.794604323946cfe64bb9a429ee827e44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb253de12a3b108db8efcd463ce06180",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be9a4015869f8b151640",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0638434aef30ce338499",
+    "url": "/static/js/2.1d628fbd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1d628fbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be9a4015869f8b151640",
+    "url": "/static/js/main.5f5f7ca1.chunk.js"
+  },
+  {
+    "revision": "717be7891cc3d466bc4e",
+    "url": "/static/js/runtime-main.b43b082e.js"
+  }
+]);
